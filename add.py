ec62807d5bf5565c8d931039434084a99b3e5af4
hi117 <hi117.contact@gmail.com>
2012-09-19 21:47:15 -0400

    pushing to master race

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-# recursively adds all files in the directory to the db
+# adds a single file to the database
 #
 # Copyright (c) 2011 Zachary Winnerman
 #
@@ -21,19 +21,14 @@
 
 import pictures as p
 from sys import argv,exit
-from subprocess import check_output
 import os.path
 
 if len(argv)!=3:
-  print("Usage: python2 addall.py idb directory")
+  print "Usage: python2 addall.py idb directory"
   exit(1)
 p._idb=p.idb.db(argv[1])
-# get a list of files in the directory
-#for i in os.listdir(argv[2]):
-for i in check_output(['find',argv[2],'-type','f']).split("\n"):
-  if os.path.isfile(i):
-    print("current path is: "+i)
+if os.path.isfile(i):
     try:
-      p.add(i,'setMe')
+        p.add(i,'setMe')
     except AssertionError as e:
-      print("match: "+i)
+        print('Already in database!')
