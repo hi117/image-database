eb9735e86e5d3fbc2894317107a2b912f2cba408
hi117 <zacharyw09264@gmail.com>
2013-10-05 08:26:05 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,6 +1,6 @@
 # takes an argument of directory and image db and removes images from the driectory already in the db
 #
-# Copyright (c) 2011 Zachary Winnerman
+# Copyright (c) 2011 hi117
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
